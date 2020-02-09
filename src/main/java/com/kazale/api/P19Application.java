26100229046790d2e951c732a9fae4e764b94961
jardeldantas <jarde.dantas@outlook.com>
2020-02-09 18:04:02 -0300

    Autenticação com JWT

--- src/main/java/com/kazale/api/P19Application.java
+++ src/main/java/com/kazale/api/P19Application.java
@@ -6,29 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class P19Application {
 	
-//	@Autowired
-//	private UsuarioRepository usuarioRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(P19Application.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner() {
-//		return args -> {
-//			
-//			Usuario usuario = new Usuario();
-//			usuario.setEmail("usuario@email.com");
-//			usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
-//			usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
-//			this.usuarioRepository.save(usuario);
-//			
-//			Usuario admin = new Usuario();
-//			admin.setEmail("admin@email.com");
-//			admin.setPerfil(PerfilEnum.ROLE_ADMIN);
-//			admin.setSenha(SenhaUtils.gerarBCrypt("123456"));
-//			this.usuarioRepository.save(admin);
-//			
-//		};
-//	}
 }
